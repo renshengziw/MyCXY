c86cac41b45d01e4ab1d42e052cc130ccfeb766f
zhangchen <357752609@qq.com>
2015-05-28 11:48:41 +0800

    sourceTree推送

---
+++ app/src/main/java/com/chen/cxy/common/Constant.java
@@ -0,0 +1,7 @@
+package com.chen.cxy.common;
+
+/**
+ * Created by Administrator on 2015/5/20.
+ */
+public class Constant {
+}
---
+++ app/src/main/java/com/chen/cxy/view/home/entity/ChatMessageEntity.java
@@ -0,0 +1,7 @@
+package com.chen.cxy.view.home.entity;
+
+/**
+ * Created by Administrator on 2015/5/20.
+ */
+public class ChatMessageEntity {
+}
